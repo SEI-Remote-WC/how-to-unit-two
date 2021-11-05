1cc083a5b1cee3f18923fb542c25cd8c9723d0ca
ManliestBen <benjamintmanley@gmail.com>
2021-11-04 19:20:50 -0500

    add models

--- models/profile.js
+++ models/profile.js
@@ -1,8 +1,25 @@
 import mongoose from 'mongoose'
+const Schema = mongoose.Schema
 
-const profileSchema = new mongoose.Schema({
+const commentSchema = new Schema({
+  content: String,
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
+}, {
+  timestamps: true
+})
+
+const pokemonSchema = new Schema ({
+  name: String,
+  weight: Number,
+  comments: [commentSchema]
+}, {
+  timestamps: true
+})
+
+const profileSchema = new Schema({
   name: String,
   avatar: String,
+  pokemon: [pokemonSchema]
 }, {
   timestamps: true
 })
